dfdfaa1c64c6086c6fb99bc972c828d8c5918463
Piyush Gupta <piyushaggarwal1221@gmail.com>
2026-03-07 00:49:06 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-const PORT=process.env.PORT || 3000;
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use('/api/auth',authRoutes);
-app.use('/api/message',messageRoutes);
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-  console.log("server is listening on port:"+PORT);
+  connectDB();
+  console.log("server is listening on port:" + PORT);
 });
