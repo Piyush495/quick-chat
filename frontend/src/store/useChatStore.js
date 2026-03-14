bd1fcb06e90d2f66d95eed25bf804f359a0cd056
Piyush Gupta <piyushaggarwal1221@gmail.com>
2026-03-15 01:10:50 +0530

    Add Profile Header

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,38 @@
+import toast from "react-hot-toast";
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore=create((set,get)=>({
+    allContacts:[],
+    chats:[],
+    messages:[],
+    activeTab:"chats",
+    selectedUser:null,
+    isUserLoading:false,
+    isMessagesLoading:false,
+
+    setActiveTab:(tab)=>set({activeTab:tab}),
+    setSelectedUser:(user)=>set({selectedUser:user}),
+    getAllContacts:async()=>{
+        set({isUserLoading:true});
+        try {
+            const res=await axiosInstance.get("/message/contacts");
+            set({allContacts:res.data});
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }finally{
+            set({isUserLoading:false})
+        }
+    },
+    getMyChatPartners:async()=>{
+        set({isUserLoading:true});
+        try {
+            const res=await axiosInstance.get("/message/chats");
+            set({chats:res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({isUserLoading:false});
+        }
+    }
+}));
