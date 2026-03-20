8cbbc5c6e85089a867b692beb385fba01f413ae0
Piyush Gupta <piyushaggarwal1221@gmail.com>
2026-03-21 00:13:11 +0530

    chore: prepare app for deployment

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    sameSite:process.env.Mode === "development" ? "strict" : "none",
+    secure: process.env.Mode !== "development",
   });
 
   return token;
